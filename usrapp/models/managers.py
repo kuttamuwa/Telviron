1b164b51806c54ac5adf99c289bbd653a65e6326
kuttamuwa <ucok.umut@gmail.com>
2021-11-03 20:28:12 +0300

    telephone removed

--- usrapp/models/managers.py
+++ usrapp/models/managers.py
@@ -8,7 +8,7 @@ class CustomUserManager(BaseUserManager):
         for authentication instead of usernames.
     """
 
-    def create_user(self, email, password, telephone, **extra_fields):
+    def create_user(self, email, password, **extra_fields):
         """
         Create and save a User with the given email and password.
         """
@@ -16,12 +16,12 @@ class CustomUserManager(BaseUserManager):
             raise ValueError('The Email must be set')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, telephone=telephone, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password, telephone, **extra_fields):
+    def create_superuser(self, email, password, **extra_fields):
         """
         Create and save a SuperUser with the given email and password.
         """
@@ -36,7 +36,7 @@ class CustomUserManager(BaseUserManager):
             raise ValueError('Superuser must have is_superuser=True.')
 
         # TODO: source of the leak on the chain...
-        return self.create_user(email, password, telephone, **extra_fields)
+        return self.create_user(email, password **extra_fields)
 
 
 class Command(BaseCommand):
