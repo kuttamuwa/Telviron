8212f144af3ab497f46c31b31e340955e556839f
kuttamuwa <ucok.umut@gmail.com>
2021-11-10 15:20:43 +0300

    add telephone

--- usrapp/management/commands/createsuperuser2.py
+++ usrapp/management/commands/createsuperuser2.py
@@ -11,13 +11,3 @@ class CustomCommand(BaseCommand):
     def add_arguments(self, parser):
         super(CustomCommand, self).add_arguments(parser)
         parser.add_argument('--telephone', help='Adds telephone')
-
-    def handle(self, *args, **options):
-        print("my handler")
-        telephone = options["telephone"]
-        username = options["username"]
-        password = options["email"]
-
-        user = CustomUser.objects.create_superuser(
-
-        )
