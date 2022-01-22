c361fd416212ac2d3f0cd3bc882862620cd74775
kuttamuwa <ucok.umut@gmail.com>
2022-01-22 23:40:40 +0300

    static added

--- notifications/controllers/apps.py
+++ notifications/controllers/apps.py
@@ -6,10 +6,16 @@ class NotificationsConfig(AppConfig):
     name = 'notifications'
 
     def ready(self):
+        """
+        azmi menguye not: ready fonksiyonu django ayaga kalktiginda calismasini istedigimiz
+        fonksiyonları çalıştırız.
+        not: Bazen bunlar iki kere çalışır..
+
+        """
         print("Notification is up !")
-        from notifications.scripts.telegram_initial import add_telegram_system_bot
-        from notifications.scheduled.telegram import tele_service
+        # from notifications.scripts.telegram_initial import add_telegram_system_bot
+        # from notifications.scheduled.telegram import tele_service
 
         # telegram setup
-        add_telegram_system_bot()
-        tele_service = tele_service
+        # add_telegram_system_bot()
+        # tele_service = tele_service
